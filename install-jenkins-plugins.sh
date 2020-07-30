f91479c92aa79315e43bf09337efa0f8cee0998f
aurcame <aurcame@gmail.com>
2020-07-31 00:37:31 +0300

    - Change NEXUS_SERVER_IP to 10.0.1.101 - add install-jenkins-plugins.sh: installing Jenkins plugins

---
+++ install-jenkins-plugins.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# copy Jenkins CLI
+wget http://10.0.1.101:8080/jnlpJars/jenkins-cli.jar
+# install neccessary plugins
+java -jar jenkins-cli.jar -s http://10.0.1.101:8080/ install-plugin parameterized-trigger \
+        http://updates.jenkins-ci.org/latest/ansicolor.hpi \
+        http://updates.jenkins-ci.org/latest/ansible.hpi \
+        http://updates.jenkins-ci.org/latest/nexus-artifact-uploader.hpi \
+        http://updates.jenkins-ci.org/latest/pipeline-multibranch-defaults.hpi \
+        http://updates.jenkins-ci.org/latest/maven-plugin.hpi \
+        http://updates.jenkins-ci.org/latest/job-dsl.hpi \
+        http://updates.jenkins-ci.org/latest/locale.hpi \
+        — username dell — password $1 –restart
