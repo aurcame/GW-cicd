3078086a523a34600a02f6d8778bf1966610c162
aurcame <aurcame@gmail.com>
2020-08-02 00:00:44 +0300

    - Change auth parameter; - Remove pipeline multibranch-defaults plugin;

--- install-jenkins-plugins.sh
+++ install-jenkins-plugins.sh
@@ -2,12 +2,11 @@
 # copy Jenkins CLI
 wget http://10.0.1.101:8080/jnlpJars/jenkins-cli.jar
 # install neccessary plugins
-java -jar jenkins-cli.jar -s http://10.0.1.101:8080/ install-plugin parameterized-trigger \
+java -jar jenkins-cli.jar -s http://10.0.1.101:8080/ --auth dell:159632 install-plugin parameterized-trigger \
         http://updates.jenkins-ci.org/latest/ansicolor.hpi \
         http://updates.jenkins-ci.org/latest/ansible.hpi \
         http://updates.jenkins-ci.org/latest/nexus-artifact-uploader.hpi \
-        http://updates.jenkins-ci.org/latest/pipeline-multibranch-defaults.hpi \
         http://updates.jenkins-ci.org/latest/maven-plugin.hpi \
         http://updates.jenkins-ci.org/latest/job-dsl.hpi \
         http://updates.jenkins-ci.org/latest/locale.hpi \
-        — username dell — password $1 –restart
+        --restart
